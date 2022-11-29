64a66c927f50c8be939edd0980831e7484993291
Adriano Florentino <adriano.florentino@bs2tecnologia.com.br>
2022-11-29 20:49:28 -0300

    Removing sass related libs, i dont even know why I even used it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import sketch from './app/sketch';
-import './scss/styles.scss';
 
 // Initialize sketch
 new p5(sketch);
--- webpack.common.js
+++ webpack.common.js
@@ -10,13 +10,6 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        }, {
             test: /\.(js)$/,
             exclude: /(node_modules)/,
             loader: 'babel-loader',
